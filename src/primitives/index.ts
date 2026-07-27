f149cb2012431df507ad6e799187deaad1ef4369
Mavis <noreply@trovy.app>
2026-07-27 15:08:16 +0000

    chore: initial commit

---
+++ src/primitives/index.ts
@@ -0,0 +1,9 @@
+export { Button, type ButtonProps } from "./Button";
+export { Card, type CardProps } from "./Card";
+export { Badge, Eyebrow, type BadgeProps } from "./Badge";
+export { Kbd } from "./Kbd";
+export { Reveal } from "./Reveal";
+export { SectionHeading, type SectionHeadingProps } from "./SectionHeading";
+export { Container, type ContainerProps } from "./Container";
+export { Stat } from "./Stat";
+export { LogoMark, Brand } from "./Logo";
